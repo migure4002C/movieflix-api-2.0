3cc0a46ab70d8d15db02f36617bde1b6ce35f433
migure4002C <miguel21bueno@gmail.com>
2024-08-21 11:44:28 -0300

    adicionada ordenaçao, genero e lingua do filme

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,21 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
 
 const port = 3000
 const app = express();
+const prisma = new PrismaClient();
 
-app.get('/movies', (req, res) => {
-    res.send('Listagem de filmes');
+app.get('/movies', async (_, res) => {
+    const movies = await prisma.movie.findMany({
+        orderBy: {
+            title: "asc",
+        },
+        include: {
+            genres: true,
+            languages: true
+        }
+    });
+    res.json(movies);
 });
 
 app.listen(port, () => {
